ade22cd363324eca0d546020d643ab34957bd8d2
Damianss127 <drobledo4@hotmail.com>
2022-04-27 10:10:52 -0700

    v0.1 Start of Regex formating Start of User Input Managment Start of Menu Displaying

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text.RegularExpressions;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,8 +9,52 @@ namespace DBMS
 {
     class Program
     {
+        
         static void Main(string[] args)
         {
+            string ver = "v0.1";
+            /* Keywords:
+             * Crea base %name
+             * Borrar base %name
+             * Muestra bases
+             * Usa base %name
+             * Crear tabla %name
+             *  %name %type %size
+             * Borrar tabla %name
+             * Borrar campo %name %name
+             *  
+             *  Notes:
+             *  tbl1.est | structure file
+             *  tbl1.dat | data file
+             */
+            Regex CB = new Regex(@"\b(crea base)\s\S+;"), BB = new Regex(@"\b(borrar base)\s\S;"), MB = new Regex(@"\b(muestra base)\s\S;"), UB = new Regex(@"\b(usa base)\s\S;"),
+                CT = new Regex(@"crear tabla \w;"), BT = new Regex(@"borrar tabla \w;"), BC = new Regex(@"borrar campo \w;");
+            string UsrInput;
+            
+
+            Info();
+            Lectura();
+
+            void Info()
+            {
+                Console.WriteLine("DBMS " + ver);
+                Console.WriteLine("Proyecto final de administracion de bases de datos");
+                Console.WriteLine("Creado por Robledo Sanchez Damian 19211719");
+                Console.Write(">");
+            }
+            void Lectura()
+            {
+                UsrInput = Console.ReadLine();
+                if (CB.IsMatch(UsrInput))
+                {
+                    CreaBase();
+                    Console.ReadLine();
+                }
+            }
+            void CreaBase()
+            {
+
+            }
         }
     }
 }
